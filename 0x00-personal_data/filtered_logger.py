473a9cc978b314ff1b994bd9cc0cf7e41cf6b58f
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-11-02 22:41:14 +0200

    solution for tasks 1->3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,13 @@ filtered logger
 """
 import re
 from typing import List
+import logging
+import os
+import mysql.connector
+
+
+PII_FIELDS: List[str] = ["name", "email", "phone", "ssn", "password"]
+PERSONAL_DATA_DB_NAME = "my_db"
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -15,3 +22,54 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """ get logger function"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ get db function"""
+    db_username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    db_password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    db_host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+
+    db_connection = mysql.connector.connect(
+        user=db_username,
+        password=db_password,
+        host=db_host,
+        database=PERSONAL_DATA_DB_NAME
+    )
+
+    return db_connection
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ init method for RedactingFormatter"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records using filter_datum"""
+        message = filter_datum(self.fields, self.REDACTION,
+                               record.getMessage(), self.SEPARATOR)
+        record.msg = message
+        return super(RedactingFormatter, self).format(record)
