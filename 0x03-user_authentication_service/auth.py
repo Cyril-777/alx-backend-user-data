cb136696471c7922211e77fc6fb207dcf9fc81aa
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-11-14 22:38:11 +0200

    Task 10 - Fix (create_session)

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -51,8 +51,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            session_id = _hash_password(user.id)
-            self._db.update_user(user.id, session_id=session_id)
-            return session_id
         except NoResultFound:
             return None
+        else:
+            user.session_id = _generate_uuid()
+            return user.session_id
