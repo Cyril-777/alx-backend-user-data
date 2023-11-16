ce0abc4660a2f2a70473bd7ac4991c9e38adb25f
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-11-16 03:49:17 +0200

    Tasks 14 & 15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,5 +46,30 @@ def login() -> str:
         abort(401)
 
 
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    """logout method to destroy a session
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
+
+
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """profile method to get the user profile
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
